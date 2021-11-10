a04b5866866b0cff6b18633b93dd22e1b47b8d8b
Qhucy <qhucy@protonmail.com>
2021-11-09 22:04:38 -0800

    Fixed import statements

--- src/test/java/com/qhucy/configmanager/source/ConfigSourceTest.java
+++ src/test/java/com/qhucy/configmanager/source/ConfigSourceTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit Testing for the ConfigSource class.
