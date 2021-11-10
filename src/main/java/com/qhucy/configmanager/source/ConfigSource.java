549da6d59e6462bd9ee10655ca1347208b63e672
Qhucy <qhucy@protonmail.com>
2021-11-09 22:01:41 -0800

    Changed name of variable source to sourcePath

--- src/main/java/com/qhucy/configmanager/source/ConfigSource.java
+++ src/main/java/com/qhucy/configmanager/source/ConfigSource.java
@@ -16,30 +16,28 @@ public class ConfigSource
     /**
      * Returns a new ConfigSource object from a given file.
      *
-     * @param source The text path to create the ConfigSource object from.
+     * @param sourcePath The text path to create the ConfigSource object from.
      * @return A new ConfigSource object from a given file.
      */
     @NotNull
-    public static ConfigSource fromString( @NotNull final String source )
+    public static ConfigSource fromString( @NotNull final String sourcePath )
     {
-        Validate.notNull( source, "Parameter source cannot be null." );
+        Validate.notNull( sourcePath, "Parameter sourcePath cannot be null." );
 
-        return new ConfigSource( source );
+        return new ConfigSource( sourcePath );
     }
 
     // The text path that directs you to the source of where the config field and value map was loaded from.
-    private String source;
+    private String sourcePath;
 
     /**
      * Instantiates a ConfigSource with a given text source.
      *
-     * @param source The text path that directs you to the source of the config field and value map.
+     * @param sourcePath The text path that directs you to the source of the config field and value map.
      */
-    public ConfigSource( @NotNull final String source )
+    public ConfigSource( @NotNull final String sourcePath )
     {
-        Validate.notNull( source, "Parameter source cannot be null." );
-
-        setSource( source );
+        setSourcePath( sourcePath );
     }
 
     /**
@@ -48,21 +46,21 @@ public class ConfigSource
      * @return The text path of the source of the config field and value map.
      */
     @NotNull
-    public String getSource()
+    public String getSourcePath()
     {
-        return source;
+        return sourcePath;
     }
 
     /**
      * Sets the text path of the source of the config field and value map.
      *
-     * @param source The text path of the source of the config field and value map.
+     * @param sourcePath The text path of the source of the config field and value map.
      */
-    public void setSource( @NotNull final String source )
+    public void setSourcePath( @NotNull final String sourcePath )
     {
-        Validate.notNull( source, "Parameter source cannot be null." );
+        Validate.notNull( sourcePath, "Parameter sourcePath cannot be null." );
 
-        this.source = source;
+        this.sourcePath = sourcePath;
     }
 
 
