7c40f02fecb61e575ae5c3d3ccdc6688a2b5ddc4
Qhucy <qhucy@protonmail.com>
2022-01-15 15:22:01 -0800

    Changed name

---
+++ src/main/java/com/qhucy/configmanager/file/ConfigSaveEmphasis.java
@@ -0,0 +1,30 @@
+package com.qhucy.configmanager.file;
+
+import com.qhucy.configmanager.ConfigManager;
+import com.qhucy.configmanager.ConfigManagerBungee;
+import com.qhucy.configmanager.ConfigManagerSpigot;
+
+/**
+ * Enumeration constants representing how a config field and value map should be saved to a file.
+ *
+ * @see ConfigManager
+ * @see ConfigFileManager
+ * @see ConfigManagerSpigot
+ * @see ConfigManagerBungee
+ * <p>
+ * MIT License - Copyright (c) 2022 Qhucy Sijyo.
+ */
+public enum ConfigSaveEmphasis
+{
+
+    /**
+     * The config field and value map will be saved using the least amount of memory possible.
+     */
+    MEMORY,
+
+    /**
+     * The config field and value map will be saved in the most easily readable way by humans.
+     */
+    READABILITY
+
+}
